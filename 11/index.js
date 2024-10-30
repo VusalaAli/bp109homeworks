06e41f597847c2f91a31ca99fb8400af838c6d6f
VusalaAli <vusaleelizade2004@gmail.com>
2024-10-30 04:50:48 +0400

    added

---
+++ 11/index.js
@@ -0,0 +1,54 @@
+
+
+
+let month = prompt("ayin adini daxil edin:");
+
+
+
+switch (month) {
+  case "january":
+  case "march":
+  case "may":
+  case "july":
+  case "august":
+  case "october":
+  case "december":
+    alert("bu ayda 31 gun var");
+    break;
+  case "april":
+  case "june":
+  case "september":
+  case "november":
+    alert("bu ayda 30 gun var") ;
+    break;
+  case "february":
+    alert("bu ayda 28 gun var");
+    break;
+  default:
+    alert("sehv yazdin, balam:))");
+}
+
+
+// let katet1 = prompt("Birinci kateti daxil edin:");
+// let katet2 = prompt("İkinci kateti daxil edin:");
+// let hipotenuz = prompt("Hipotenuzu daxil edin:");
+
+
+// if (katet1 ** 2 + katet2 ** 2 === hipotenuz ** 2) {
+//   alert("Result: duzbucaqli uchbucaqdir");
+// } else {
+//   alert("Result: duzbucaqli uchbucaq deyil");
+// }
+
+
+// let number = prompt("uchreqemli eded daxil edin:");
+
+
+// let yuzluk = (number / 100);
+// let onluq = ((number % 100) / 10);
+// let birlik = (number % 110);
+
+
+// alert(yuzluk, onluq, birlik);
+
+
